2e5e88ab4eeed9083b548e169a6410fd9f904fb5
Abdoulaye Sy <doulsy2021@gmail.com>
2022-08-26 14:44:37 +0000

    1er depot

---
+++ README.md
@@ -0,0 +1,1 @@
+# APIErrorNote
---
+++ src/test/java/com/syadama/APIErrorNote/ApiErrorNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syadama.APIErrorNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiErrorNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
